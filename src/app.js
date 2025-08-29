1bfef0dfc2e6f4db7ac4dfe9116d73eb7ca6f346
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-08-29 04:53:42 +0000

    agg

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import "bootstrap";
 import "./style.css";
 
+const palo = ["corazon", "pica", "trebol", "diamante"];
+const simbol = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let simbolUp = document.querySelector("#simbolUp");
+let num = document.querySelector("#num");
+let simbolDown = document.querySelector("#simbolDown");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function cardSelect() {
+  let card = [];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function() {  //write your code here
 };

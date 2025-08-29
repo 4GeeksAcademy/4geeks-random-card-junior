c918a9d5137c4c322d3e8883ad0bdf00947296b9
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-08-29 05:31:58 +0000

    funcionabilidad js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,54 @@ let simbolUp = document.querySelector("#simbolUp");
 let num = document.querySelector("#num");
 let simbolDown = document.querySelector("#simbolDown");
 
-function cardSelect() {
+function cardSelect(array1, array2) {
   let card = [];
+  card.push(array1[Math.floor(Math.random() * array1.length)]);
+  card.push(array2[Math.floor(Math.random() * array2.length)]);
+  return card;
 }
 
-window.onload = function() {  //write your code here
+function asigColor(array) {
+  if(array[0] === "corazon" || array[0] === "diamante") {
+    simbolUp.style.color = "red";
+    num.style.color = "red";
+    simbolDown.style.color = "red";
+  } else {
+    simbolUp.style.color = "black";
+    num.style.color = "black";
+    simbolDown.style.color = "black";
+  }
+}
+
+function asigPalo(array) {
+  if(array[0] === "corazon") {
+    simbolUp.innerHTML = "♥";
+    simbolDown.innerHTML = "♥";
+  } else if(array[0] === "diamante") {
+    simbolUp.innerHTML = "♦";
+    simbolDown.innerHTML = "♦";
+  } else if(array[0] === "trebol") {
+    simbolUp.innerHTML = "♣";
+    simbolDown.innerHTML = "♣";
+  } else {
+    simbolUp.innerHTML = "♠";
+    simbolDown.innerHTML = "♠";
+  }
+}
+
+function asigSimbol(array) {
+  num.innerHTML = array[1];
+}
+
+function cardGene() {
+  let carta = cardSelect(palo, simbol);
+  asigColor(carta);
+  asigPalo(carta);
+  asigSimbol(carta);
+}
+window.onload = function() { //write your code here
+  cardGene();
 };
+document.getElementById("button").addEventListener("click", function() {
+  cardGene();
+});
